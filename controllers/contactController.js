07e0ca45b568da8e5e1742877bef7c83d3b56bfa
pralaya kumar samal <pralayasamal486@gmail.com>
2026-07-10 10:59:33 +0530

    commet the nodemailer

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -16,26 +16,26 @@ const saveContact = async (req, res) => {
 
     await contact.save(); 
 
-    const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+//     const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
 
-await transporter.sendMail({
-  from: process.env.EMAIL_USER,
-  to: process.env.EMAIL_USER,
-  subject: "New Portfolio Contact",
-  html:
-   `<h2>New Message</h2>
-    <p><b>Name:</b> ${name}</p>
-    <p><b>Email:</b> ${email}</p>
-    <p><b>Message:</b> ${message}</p>
-  ,`
+// await transporter.sendMail({
+//   from: process.env.EMAIL_USER,
+//   to: process.env.EMAIL_USER,
+//   subject: "New Portfolio Contact",
+//   html:
+//    `<h2>New Message</h2>
+//     <p><b>Name:</b> ${name}</p>
+//     <p><b>Email:</b> ${email}</p>
+//     <p><b>Message:</b> ${message}</p>
+//   ,`
     
-});
+// });
     
 
     res.status(201).json({
